91bf6bef7e3afbe2d80b0176f4a5ac3163f0d149
nerett <pt-mvb@inbox.lv>
2023-04-02 19:38:16 +0300

    added asm code examples

---
+++ examples/code_1.asm
@@ -0,0 +1,43 @@
+strt
+push 1111
+push 2222
+push 3333
+push 4444
+push 5555
+push 6666
+push 7777
+push 8888
+push 9999
+push 1234
+push ax
+push ax
+push ax
+push ax
+push ax
+push ax
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+add
+out
+pop bx
+pop [313131]
+pop ax
+pop cx
+pop [1337]
+push 99999999
+out
+hlt
---
+++ examples/code_2.asm
@@ -0,0 +1,10 @@
+strt
+push 16
+push 4
+div
+push 2
+mul
+push 10
+sub
+out
+hlt
